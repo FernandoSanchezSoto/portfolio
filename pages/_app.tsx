c4b27bac70127a8c6e0d7b96a487bce57c6b2273
Fernando Sanchez Soto <fernandosoto0643@gmail.com>
2026-03-24 16:52:54 +0000

    feat: add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import type { AppProps } from "next/app";
 import "@/styles/globals.css";
 import Head from "next/head";
-import Head from 'next/head';
 
 <Head>
   <link rel="icon" href="/fss.png" />
